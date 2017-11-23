a3b51d914a565056a9a781fe1523f1a7a8383813
Olena Ageyeva <eageeva@hotmail.com>
2017-11-23 15:11:33 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,9 +1,9 @@
 module Players
   class Computer<Player
     def move(board)
-      #index=rand(9)
-      ai_move=Game.new(Players::Computer.new("X"),Computer.new("O"), board).tap{|g| g.minimax(board)}
-      index=ai_move
+      index=rand(9)
+      #ai_move=Game.new(Players::Computer.new("X"),Computer.new("O"), board).tap{|g| g.minimax(board)}
+      #index=ai_move
       return index.to_s if board.valid_move?(index)
     end
   end
