5c665ad422cf5ed45730a7a511056812b2753cbf
Olena Ageyeva <eageeva@hotmail.com>
2017-11-23 15:00:16 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,8 @@
 module Players
   class Computer<Player
     def move(board)
-      index=rand(9)
+      #index=rand(9)
+      index=minimax(board)
       return index.to_s if board.valid_move?(index)
     end
   end
